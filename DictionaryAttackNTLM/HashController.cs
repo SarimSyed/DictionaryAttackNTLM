1a76cce7fc0575a385425a8de882f806c0018c28
SarimSyed <1783364@johnabbottcollege.net>
2022-10-13 14:21:38 -0400

    Add project files.

---
+++ DictionaryAttackNTLM/HashController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DictionaryAttackNTLM
+{
+    //String sent into static class shouls be able to be hashed and return the hashed value
+    class HashController
+    {
+        private string sourceData;
+        private byte[] tmpSource, tmpHash;
+
+        public HashController(string attackHash)
+        {
+            //Might not need the original string but it doesnt hurt to keep it
+            sourceData = attackHash;
+            tmpSource = ASCIIEncoding.ASCII.GetBytes(attackHash);
+            //Compute hash based on source data.
+            tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+        }
+
+        //Readonly
+        public string SourceData {
+
+            get
+            {
+                return sourceData;
+            }
+        
+        }
+
+        public string Hash
+        {
+            get
+            {
+                return ByteArrayToString(tmpHash);
+            }
+        }
+
+        static string ByteArrayToString(byte[] arrInput)
+        {
+            int i;
+            StringBuilder sOutput = new StringBuilder(arrInput.Length);
+            for (i = 0; i < arrInput.Length; i++)
+            {
+                sOutput.Append(arrInput[i].ToString("X2"));
+            }
+            return sOutput.ToString();
+        }
+
+
+    }
+}
---
+++ DictionaryAttackNTLM/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DictionaryAttackNTLM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string HASH_TO_ATTACK = "45e47406582741ad9fac8b0fa661b4a";
+
+
+            //Get the dictionary content
+            string dictionaryTemp;
+            //below retrieves the first dictionary
+            using (WebClient client = new WebClient())
+            {
+                dictionaryTemp = client.DownloadString("https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words");
+            }
+
+            //Splits words by newline
+            string[] currentDictionary =dictionaryTemp.Split(new string[] { Environment.NewLine }, StringSplitOptions.None) ;
+
+            string sourceData;
+            byte[] tmpSource, tmpHash;
+
+            
+            for (int i = 0; i < currentDictionary.Length; i++)
+            {
+                tmpSource = ASCIIEncoding.ASCII.GetBytes(currentDictionary[i]);
+                //Compute hash based on source data.
+                tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            }
+            
+        }
+    }
+    static string ByteArrayToString(byte[] arrInput)
+    {
+        int i;
+        StringBuilder sOutput = new StringBuilder(arrInput.Length);
+        for (i = 0; i < arrInput.Length; i++)
+        {
+            sOutput.Append(arrInput[i].ToString("X2"));
+        }
+        return sOutput.ToString();
+    }
+}
