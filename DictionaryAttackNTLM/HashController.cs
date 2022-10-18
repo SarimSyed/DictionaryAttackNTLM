db43d9692adb04aae66efe567d6ed0cd3aa6258b
Sarim <78218025+SarimSyed@users.noreply.github.com>
2022-10-18 13:59:51 -0400

    Added everything

--- DictionaryAttackNTLM/HashController.cs
+++ DictionaryAttackNTLM/HashController.cs
@@ -7,26 +7,61 @@ using System.Text;
 namespace DictionaryAttackNTLM
 {
     //String sent into static class shouls be able to be hashed and return the hashed value
+    //Should compare returned Hash with the Hash we are looking for
     class HashController
     {
-        private string sourceData;
+        const string NTLM_HASH = "NTLM";
+        const string MD5_HASH = "MD5";
+        private string sourceData, _hash;
         private byte[] tmpSource, tmpHash;
 
-        public HashController(string attackHash)
+
+        public HashController(string attackHash, string hashType = MD5_HASH)
         {
-            //Might not need the original string but it doesnt hurt to keep it
             sourceData = attackHash;
+            if (hashType == MD5_HASH)
+            {
+
+                using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+                {
+                    tmpSource = System.Text.Encoding.ASCII.GetBytes(sourceData);
+                    tmpHash = md5.ComputeHash(tmpSource);
+
+                    _hash = ByteArrayToString(tmpHash);
+
+                }
+
+
+                
+            }
+            else
+            {
+
+            //Might not need the original string but it doesnt hurt to keep it
+            
             tmpSource = ASCIIEncoding.ASCII.GetBytes(attackHash);
             //Compute hash based on source data.
             tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            _hash = ByteArrayToString(tmpHash);
+
+
+            }
+
+
         }
 
-        //Readonly
+
+
+        //Properties
         public string SourceData {
 
             get
             {
                 return sourceData;
+            }
+            set
+            {
+                sourceData = value;
             }
         
         }
@@ -35,7 +70,7 @@ namespace DictionaryAttackNTLM
         {
             get
             {
-                return ByteArrayToString(tmpHash);
+                return _hash;
             }
         }
 
--- DictionaryAttackNTLM/Program.cs
+++ DictionaryAttackNTLM/Program.cs
@@ -2,48 +2,214 @@
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
 
 namespace DictionaryAttackNTLM
 {
     class Program
     {
+        const int CHARS_IN_HASH = 32;
+        const string PASSWORD_FILE_PATH = "FoundPassword.txt";
         static void Main(string[] args)
         {
-            const string HASH_TO_ATTACK = "45e47406582741ad9fac8b0fa661b4a";
+
+            //Add links to the array to add dictionaries
+            string[] DICTIONARIES = new string[] { "https://raw.githubusercontent.com/mikejakobsen/dictionary-attack/master/10_million_password_list_top_1000000.txt",
+                 "https://raw.githubusercontent.com/mikejakobsen/dictionary-attack/master/10_million_password_list_top_1000000.txt"};
+            string foundPassword = "Not found";
+            bool isFound = false;
+            bool validInput;
+            const string NTLM_HASH = "NTLM";
+            const string MD5_HASH = "MD5";
+            const string HASH_TO_ATTACK = "45e47406582741ad9fac8b0fa661b4a3";
+            const string INPUT_FILE_NAME = "AttackHash.txt";
+            const string HASH_MENU = "Select whether your hash is MD5 or NTLM\n" +
+                "1. MD5\n" +
+                "2. NTLM\n";
+            
+            int userChoice;
+            string hashType = MD5_HASH;
+            
+            string inputHash;
+            Stopwatch timer = new Stopwatch();
+
+            //Reading from files
+
+            if (File.Exists(INPUT_FILE_NAME))
+            {
+                
+                Console.WriteLine("AttackHash file found, reading content.");
+                string[] lines = File.ReadAllLines("./attackHash.txt");
+                inputHash = lines[0].ToUpper();
+                Console.WriteLine("Content of file: " + inputHash);
+                Console.WriteLine();
+
+                if (lines[0].Length != CHARS_IN_HASH)
+                {
+                    Console.WriteLine("Hash in attackHash file invalid. Hashes must be 32 character long. Close the program and edit the txt file or continue and enter it manually.");
+                    Console.WriteLine("Press enter to continue.");
+                    Console.ReadKey();
+                   
+                    inputHash = GetUserInput();
+                    Console.WriteLine();
+                    
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(INPUT_FILE_NAME))
+                {
+                    writer.WriteLine("Replace this line with a hash");
+                }
+                Console.WriteLine();
+                inputHash = GetUserInput();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(HASH_MENU);
+            validInput = int.TryParse(Console.ReadLine(), out userChoice);
 
 
-            //Get the dictionary content
+            while (!validInput)
+            {
+                Console.WriteLine("Invalid Input, try again.");
+                Console.WriteLine();
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine(HASH_MENU);
+                validInput = int.TryParse(Console.ReadLine(), out userChoice);
+
+            }
+
+
+
+            switch (userChoice)
+            {
+                case 1:
+                    hashType = MD5_HASH;
+                    break;
+                case 2:
+                    hashType = NTLM_HASH;
+                    break;
+
+                default:
+                    validInput = false;
+                    break;
+            }
+
+
+
+            //List
+
+            List<string> hashList = new List<string>();
+
+            HashController hasher;
+
+            for (int i = 0; i < DICTIONARIES.Length; i++)
+            {
+
+                Console.WriteLine("Searching in dictionary {0}", i+1);
+                timer.Start();
+                Console.WriteLine();
+
+                string[] currentDictionary = GetDictionaryFromURL(DICTIONARIES[i]);
+                //Console.WriteLine(currentDictionary.Length);
+
+                for (int x = 0; x < currentDictionary.Length; x++)
+                {
+                    //Console.WriteLine("In second for loop");
+                    hasher = new HashController(currentDictionary[x], hashType);
+                   // Console.WriteLine(currentDictionary.Length);
+                    string currentHash = hasher.Hash;
+
+                    //Console.WriteLine("Current Hash" + currentHash);
+                    if (currentHash == HASH_TO_ATTACK.ToUpper())
+                    {
+                        Console.WriteLine("Dictionary attack successful!");
+                        Console.WriteLine();
+                        foundPassword = currentDictionary[x];
+                        isFound = true;
+                        
+                    }
+                    
+                    
+                }
+
+
+                if (isFound)
+                {
+                    timer.Stop();
+                    break;
+                }
+
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("Dictionary attack unsuccessfull");
+                Console.WriteLine("Password not found");
+            }
+
+            if (isFound)
+            {
+                Console.WriteLine("Password is :");
+                Console.WriteLine(foundPassword);
+                WriteToTxt(foundPassword);
+            }
+            double elapsedTime = timer.ElapsedMilliseconds;
+            elapsedTime = elapsedTime / 1000;
+
+
+            Console.WriteLine($"Execution Time: {elapsedTime} Seconds");
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+        //Retrieves list of words from online and returns an array
+        static string[] GetDictionaryFromURL(string url)
+        {
             string dictionaryTemp;
-            //below retrieves the first dictionary
             using (WebClient client = new WebClient())
             {
-                dictionaryTemp = client.DownloadString("https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words");
+                dictionaryTemp = client.DownloadString(url);
             }
-
-            //Splits words by newline
-            string[] currentDictionary =dictionaryTemp.Split(new string[] { Environment.NewLine }, StringSplitOptions.None) ;
-
-            string sourceData;
-            byte[] tmpSource, tmpHash;
-
+            string[] dictionaryArray = dictionaryTemp.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
             
-            for (int i = 0; i < currentDictionary.Length; i++)
-            {
-                tmpSource = ASCIIEncoding.ASCII.GetBytes(currentDictionary[i]);
-                //Compute hash based on source data.
-                tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
-            }
-            
+            return dictionaryArray;
         }
-    }
-    static string ByteArrayToString(byte[] arrInput)
-    {
-        int i;
-        StringBuilder sOutput = new StringBuilder(arrInput.Length);
-        for (i = 0; i < arrInput.Length; i++)
+
+        static string GetUserInput()
         {
-            sOutput.Append(arrInput[i].ToString("X2"));
+
+            bool invalidInput = true;
+            string hash = string.Empty;
+
+
+            while (hash.Length != CHARS_IN_HASH)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter hash for dictionary attack (valid hashes are 32 character long): ");
+                hash = Console.ReadLine().ToUpper();
+                Console.WriteLine();
+
+
+            }
+            return hash;
+
         }
-        return sOutput.ToString();
+
+        static void WriteToTxt(string pass)
+        {
+            using (StreamWriter writer = new StreamWriter(PASSWORD_FILE_PATH))
+            {
+                writer.WriteLine(pass);
+            }
+        }
     }
+
 }
